01bf26904c52088abbf006ec395c8850b0138672
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-12-09 11:18:22 +0700

    user decision voter with post object processor

--- src/main/java/com/example/authtoken/auth/SecurityConfig.java
+++ src/main/java/com/example/authtoken/auth/SecurityConfig.java
@@ -2,21 +2,14 @@ package com.example.authtoken.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.vote.AuthenticatedVoter;
-import org.springframework.security.access.vote.RoleVoter;
-import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
@@ -34,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(new TokenAuthenticationEntryPoint(), authenticationManager());
         http.authenticationProvider(provider)
                 .addFilterAfter(filter, LogoutFilter.class)
-                .authorizeRequests().anyRequest().fullyAuthenticated().accessDecisionManager(accessDecisionManager())
+                .authorizeRequests().anyRequest().fullyAuthenticated().withObjectPostProcessor(createObjectPostProcessor())
                 .and()
                 .httpBasic().disable()
                 .formLogin().disable()
@@ -46,21 +39,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    private ObjectPostProcessor<AffirmativeBased> createObjectPostProcessor() {
+        return new ObjectPostProcessor<AffirmativeBased>() {
+
+            @Override
+            public <O extends AffirmativeBased> O postProcess(O affirmativeBased) {
+                affirmativeBased.getDecisionVoters().add(new UriAccessDecisionVoter());
+                return affirmativeBased;
+            }
+        };
+    }
+
     @Autowired
     public void setProvider(TokenAuthenticationProvider provider) {
         this.provider = provider;
     }
 
-    private AccessDecisionManager accessDecisionManager() {
-        List<AccessDecisionVoter<? extends Object>> decisionVoters
-                = Arrays.asList(
-                new WebExpressionVoter(),
-                new RoleVoter(),
-                new AuthenticatedVoter(),
-                new UriAccessDecisionVoter()
-
-        );
-        return new UnanimousBased(decisionVoters);
-    }
 }
 
