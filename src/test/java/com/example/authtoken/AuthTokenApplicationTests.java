460c90f0f70885642ffcde24165fa60b110adae6
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-12-09 21:35:19 +0700

    use junit 5

--- src/test/java/com/example/authtoken/AuthTokenApplicationTests.java
+++ src/test/java/com/example/authtoken/AuthTokenApplicationTests.java
@@ -6,12 +6,9 @@ import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthTokenApplicationTests {
 
