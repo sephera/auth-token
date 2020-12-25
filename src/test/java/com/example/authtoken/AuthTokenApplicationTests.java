154f4c1ca355171d7a5e3a8b0322a7dda608ebfb
Hai Nguyen <hai@fuvi.vn>
2020-12-25 17:57:40 +0700

    Upgrade spring boot 2.4.1

--- src/test/java/com/example/authtoken/AuthTokenApplicationTests.java
+++ src/test/java/com/example/authtoken/AuthTokenApplicationTests.java
@@ -27,44 +27,44 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 
 @SpringBootTest
-public class AuthTokenApplicationTests {
+class AuthTokenApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	public void createToken() {
-		String secret = "21ff04df-889b-4f4e-8930-cf0fad57df0f";
-		Algorithm algorithm = Algorithm.HMAC256(secret);
-		String token = JWT.create().withIssuer("auth-token")
-				.withArrayClaim("authority", new String[] { "ADMIN", "USER" })
-				.withArrayClaim("uri", new String[] { "/user", "/actuator/health" }).sign(algorithm);
+    @Test
+    void createToken() {
+        String secret = "21ff04df-889b-4f4e-8930-cf0fad57df0f";
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        String token = JWT.create().withIssuer("auth-token")
+                .withArrayClaim("authority", new String[]{"ADMIN", "USER"})
+                .withArrayClaim("uri", new String[]{"/user", "/actuator/health"}).sign(algorithm);
 
-		System.out.println(token);
+        System.out.println(token);
 
-		Assert.assertNotNull(token);
-	}
+        Assertions.assertNotNull(token);
+    }
 
-	@Test
-	public void verifyToken() {
-		String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdHkiOlsiQURNSU4iLCJVU0VSIl0sImlzcyI6ImF1dGgtdG9rZW4iLCJ1cmkiOlsiL3VzZXIiLCIvYWN0dWF0b3IvaGVhbHRoIl19._BCoNG-wN93B3DvkCVXaPNOYe8GeXjX6fxfDuwCTdbE";
-		String secret = "21ff04df-889b-4f4e-8930-cf0fad57df0f";
-		Algorithm algorithm = Algorithm.HMAC256(secret);
-		JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth-token").build();
-		DecodedJWT jwt = verifier.verify(token);
-		final Claim claim = jwt.getClaim("authority");
-		final String[] authorities = claim.asArray(String.class);
-		System.out.println(Arrays.toString(authorities));
+    @Test
+    void verifyToken() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdHkiOlsiQURNSU4iLCJVU0VSIl0sImlzcyI6ImF1dGgtdG9rZW4iLCJ1cmkiOlsiL3VzZXIiLCIvYWN0dWF0b3IvaGVhbHRoIl19._BCoNG-wN93B3DvkCVXaPNOYe8GeXjX6fxfDuwCTdbE";
+        String secret = "21ff04df-889b-4f4e-8930-cf0fad57df0f";
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth-token").build();
+        DecodedJWT jwt = verifier.verify(token);
+        final Claim claim = jwt.getClaim("authority");
+        final String[] authorities = claim.asArray(String.class);
+        System.out.println(Arrays.toString(authorities));
 
-		Assert.assertNotNull(authorities);
-	}
+        Assertions.assertNotNull(authorities);
+    }
 
 }
